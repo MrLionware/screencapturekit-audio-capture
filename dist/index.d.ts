1565b0955d73e8b2fba2d92ca98868af0971539c
Caleb Rubiano <kacklion0101@gmail.com>
2025-11-27 12:45:48 -0500

    build: export multi-capture option types from main index
    Add MultiAppCaptureOptions, MultiWindowCaptureOptions, and MultiDisplayCaptureOptions to type exports in dist/index.d.ts.

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -11,6 +11,6 @@ export { STTConverter, ScreenCaptureKit } from './utils';
 export { AudioCaptureError, ErrorCode, ErrorCodes } from './core/errors';
 export { cleanupAll, getActiveInstanceCount, installGracefulShutdown } from './core/cleanup';
 export type { CleanupResult } from './core/cleanup';
-export type { ApplicationInfo, WindowInfo, WindowFrame, DisplayInfo, AudioFormat, AudioSample, NativeAudioSample, CaptureTargetType, CaptureInfo, CaptureOptions, NativeCaptureConfig, GetApplicationsOptions, GetWindowsOptions, GetAudioAppsOptions, SelectAppOptions, ActivityTrackingOptions, ProcessActivityInfo, ActivityInfo, PermissionStatus, CaptureStatus, AudioStreamOptions, STTStreamOptions, WavOptions, AppIdentifier, } from './core/types';
+export type { ApplicationInfo, WindowInfo, WindowFrame, DisplayInfo, AudioFormat, AudioSample, NativeAudioSample, CaptureTargetType, CaptureInfo, CaptureOptions, NativeCaptureConfig, GetApplicationsOptions, GetWindowsOptions, GetAudioAppsOptions, SelectAppOptions, ActivityTrackingOptions, ProcessActivityInfo, ActivityInfo, PermissionStatus, CaptureStatus, AudioStreamOptions, STTStreamOptions, WavOptions, AppIdentifier, MultiAppCaptureOptions, MultiWindowCaptureOptions, MultiDisplayCaptureOptions, } from './core/types';
 export { AudioCapture as default } from './capture';
 //# sourceMappingURL=index.d.ts.map
